3ef44bc6e73859e5a2f0b26c83a2728d9303b1b8
Kamal Singh <37046527+CodingStuff-GitHub@users.noreply.github.com>
2022-11-12 16:49:09 +0530

    Update 1. HashTable.py

--- Arrays and Strings/isUnique/1. HashTable.py
+++ Arrays and Strings/isUnique/1. HashTable.py
@@ -1,11 +1,11 @@
-# Hash Table
-def is_unique(str):
+# Hash Table  - O(n)
+def is_unique(input_str):
     # This will help to create the difference when checking in the hash table
     f = ord('a')
     # It will contain key value of the elements that are coming in the nums
     hashmap = [0 for _ in range(ord('a'), ord('z') + 1)]
     # Looping through the array
-    for i in str:
+    for i in input_str:
         key = ord(i) - f
         # If hashmap contains the i key  =  1 then it is not unique i.e it already came before
         if hashmap[key] == 1:

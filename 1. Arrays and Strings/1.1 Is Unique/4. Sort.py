4230fb746e958f71ebec7cf6fcfe426ed61e4371
Kamal Singh <37046527+CodingStuff-GitHub@users.noreply.github.com>
2022-11-16 17:11:38 +0530

    Sonarlint

--- 1. Arrays and Strings/1.1 Is Unique/4. Sort.py
+++ 1. Arrays and Strings/1.1 Is Unique/4. Sort.py
@@ -3,11 +3,8 @@ def is_unique(input_str):
     # Sort the Input string
     input_str = ''.join(sorted(input_str))
     # Loop through the string start from 1st character (Not from zero because we are subtracting 1 from the index)
-    for i in range(1, len(input_str)):
-        # Check if previous character is same as current character
-        if input_str[i] == input_str[i - 1]:
-            return False
-    return True
+    # Check if previous character is same as current character
+    return all(input_str[i] != input_str[i - 1] for i in range(1, len(input_str)))
 
 
 print(is_unique("abcd"))
--- 1. Arrays and Strings/1.2 Check Permutation/2. Sort.py
+++ 1. Arrays and Strings/1.2 Check Permutation/2. Sort.py
@@ -4,7 +4,4 @@ def check_permutation(input_string_1, input_string_2):
     input_string_1 = ''.join(sorted(input_string_1))
     input_string_2 = ''.join(sorted(input_string_2))
     # Compare the elements of both strings and return false if they are difference
-    for i in range(0, len(input_string_1)):
-        if input_string_1[i] != input_string_2[i]:
-            return False
-    return True
+    return all(input_string_1[i] == input_string_2[i] for i in range(len(input_string_1)))

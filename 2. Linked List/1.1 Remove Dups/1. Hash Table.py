e79d6201fb3abd6eec08fb34fcf12b77389dbaba
Kamal Singh <37046527+CodingStuff-GitHub@users.noreply.github.com>
2022-11-19 01:08:14 +0530

    Create 1. Hash Table.py
    Sample for all

---
+++ 2. Linked List/1.1 Remove Dups/1. Hash Table.py
@@ -0,0 +1,11 @@
+from utils import create_list, print_list
+
+
+def remove_dups(head):
+    pass
+
+
+head = create_list([3, 4, 3, 5, 6, 7, 3, 1, 6, 8])
+print_list(head)
+remove_dups(head)
+print_list(head)

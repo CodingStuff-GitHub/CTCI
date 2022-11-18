4847defb483e2575c83cafeba124579ed5f6e426
Kamal Singh <37046527+CodingStuff-GitHub@users.noreply.github.com>
2022-11-19 01:51:00 +0530

    Create 2. No HashSet.py

---
+++ 2. Linked List/1.1 Remove Dups/2. No HashSet.py
@@ -0,0 +1,22 @@
+# Double pointer just like double loops
+from utils import create_list, print_list
+
+
+def remove_dups(head):
+    i = head
+    while i is not None:
+        prev = i
+        j = i.next
+        while j is not None:
+            if i.val != j.val:
+                prev = j
+            else:
+                prev.next = j.next
+            j = j.next
+        i = i.next
+
+
+head = create_list([1, 3, 4, 3, 5, 6, 7, 3, 1, 6, 8, 5])
+print_list(head)
+remove_dups(head)
+print_list(head)

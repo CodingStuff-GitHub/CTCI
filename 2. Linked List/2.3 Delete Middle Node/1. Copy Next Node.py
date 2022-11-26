170fb4ae6279b72d41d5a5870e62eddf9bfa1d13
Kamal Singh <37046527+CodingStuff-GitHub@users.noreply.github.com>
2022-11-26 15:14:10 +0530

    Update 1. Copy Next Node.py

--- 2. Linked List/2.3 Delete Middle Node/1. Copy Next Node.py
+++ 2. Linked List/2.3 Delete Middle Node/1. Copy Next Node.py
@@ -2,18 +2,18 @@
 from utils import create_list, print_list
 
 
-def copy_next_node(middleNode):
-    if middleNode.next is not None:
+def copy_next_node(middle_node):
+    if middle_node.next is not None:
         # Next Value should be put in the current node
-        middleNode.val = middleNode.next.val
+        middle_node.val = middle_node.next.val
         # Curret Node next will point to next Node's next
-        middleNode.next = middleNode.next.next
+        middle_node.next = middle_node.next.next
 
 
 head = create_list([1, 3, 4, 5, 6, 7, 9, 11, 98])
-# Lets say we want to delete 5
-middleNode = head.next.next.next.next
+# Lets say we want to delete 6
+middle_node = head.next.next.next.next
 print_list(head)
 # middleNode is passed not the head of the list
-copy_next_node(middleNode)
+copy_next_node(middle_node)
 print_list(head)

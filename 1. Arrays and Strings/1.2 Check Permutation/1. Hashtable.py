8593951b40f27d12156830719d52bf8e0c424882
Kamal Singh <37046527+CodingStuff-GitHub@users.noreply.github.com>
2022-11-29 19:00:28 +0530

    Update 1. Hashtable.py
    mistake corrected

--- 1. Arrays and Strings/1.2 Check Permutation/1. Hashtable.py
+++ 1. Arrays and Strings/1.2 Check Permutation/1. Hashtable.py
@@ -6,17 +6,19 @@ def check_permutation(input_string_1, input_string_2):
     f = ord('a')
     # hash table for 26 characters
     hashmap = [0 for _ in range(ord('a'), ord('z') + 1)]
-    # Loop through each character and put it in hash table
+    # Loop through each character and increment it in hash table (remember to count frequencies)
     for i in input_string_1:
         index = ord(i) - f
-        hashmap[index] = 1
-    # Loop through each character and check if it is in hash table
+        hashmap[index] += 1
+    # Loop through each character and check if it is in hash table but after subtracting one frequency
     for j in input_string_2:
         index = ord(j) - f
-        if hashmap[index] != 1:
+        hashmap[index] -= 1
+        if hashmap[index] < 0:
             return False
     return True
 
 
 print(check_permutation("abcd", "acdb"))
 print(check_permutation("abcde", "afcdb"))
+print(check_permutation("aacc", "ccac"))

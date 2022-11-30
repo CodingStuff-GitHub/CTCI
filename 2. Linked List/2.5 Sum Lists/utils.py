89a0bffee588b72d6a1e70d2606fc9e973114df2
Kamal Singh <37046527+CodingStuff-GitHub@users.noreply.github.com>
2022-11-30 18:00:40 +0530

    Create utils.py

---
+++ 2. Linked List/2.5 Sum Lists/utils.py
@@ -0,0 +1,34 @@
+class Node:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+def create_list(arr):
+    head = Node(arr[0], None)
+    point = head
+    for i in range(1, len(arr)):
+        temp = Node(arr[i], None)
+        point.next = temp
+        point = point.next
+    return head
+
+
+def append_at_tail(head, val):
+    if head is None:
+        head = Node(val, None)
+    else:
+        temp = head
+        while temp.next is not None:
+            temp = temp.next
+        temp.next = Node(val, None)
+    return head
+
+
+def print_list(ll):
+    arr = []
+    while ll is not None:
+        arr.append(ll.val)
+        ll = ll.next
+    print(" -> ".join([str(i) for i in arr]))
+    return arr

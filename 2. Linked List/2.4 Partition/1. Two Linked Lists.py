47a2469869f49eb3e105cc7eb903fa5c0016301f
Kamal Singh <37046527+CodingStuff-GitHub@users.noreply.github.com>
2022-11-30 15:11:22 +0530

    Create 1. Two Linked Lists.py

---
+++ 2. Linked List/2.4 Partition/1. Two Linked Lists.py
@@ -0,0 +1,28 @@
+from utils import create_list, print_list, append_at_tail
+
+
+# Two new Linked List which will take smaller and greater(or equal) elements
+# then we will merge them together by adding last element next to head of second linked list
+def two_linked_list(head, part):
+    # h1 is for greater(or equal) elements
+    # h2 is for less elements
+    h1, h2 = None, None
+    while head is not None:
+        if head.val >= part:
+            h1 = append_at_tail(h1, head.val)
+        else:
+            h2 = append_at_tail(h2, head.val)
+        head = head.next
+    # Going through h2 linked list to get to the last element
+    head = h2
+    while head.next is not None:
+        head = head.next
+    # Last element next connects to h1 linked list head
+    head.next = h1
+    return h2
+
+
+head = create_list([3, 5, 6, 8, 10, 1, 2, 12, 5, 3, 8])
+print_list(head)
+head = two_linked_list(head, 5)
+print_list(head)

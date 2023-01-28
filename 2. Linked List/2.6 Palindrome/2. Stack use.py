67f797cda34f057aa09cbfb31811c29f51513665
Kamal Singh <37046527+CodingStuff-GitHub@users.noreply.github.com>
2023-01-28 15:09:59 +0530

    Create 2. Stack use.py

---
+++ 2. Linked List/2.6 Palindrome/2. Stack use.py
@@ -0,0 +1,31 @@
+from utils import create_list, print_list
+
+
+def palindrome_check(head):
+    slow = head
+    fast = head.next
+    stack = [slow]
+    # Loop for runner and put it in stack
+    while fast.next is not None and fast.next.next is not None:
+        slow = slow.next
+        fast = fast.next.next
+        stack.append(slow)
+    # Check for odd if yes do double jump or else single jump
+    slow = slow.next.next if fast.next is not None else slow.next
+    # 2nd Loop continues ith the slow runner
+    while slow is not None:
+        if stack.pop().val != slow.val:
+            return False
+        slow = slow.next
+    return True
+
+
+head = create_list([20, 7, 4, 4, 7, 20])
+print_list(head)
+print(palindrome_check(head))
+head = create_list([20, 7, 4, 4, 7, 20, 1])
+print_list(head)
+print(palindrome_check(head))
+head = create_list([20, 7, 4, 1, 4, 7, 20])
+print_list(head)
+print(palindrome_check(head))

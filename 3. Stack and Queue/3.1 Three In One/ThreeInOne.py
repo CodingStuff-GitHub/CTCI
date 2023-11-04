7b94a5f28bbf8b7c390ece2ee3ca667ead7ae5f2
Kamal Singh <37046527+CodingStuff-GitHub@users.noreply.github.com>
2023-11-04 21:07:23 +0530

    Update ThreeInOne.py

--- 3. Stack and Queue/3.1 Three In One/ThreeInOne.py
+++ 3. Stack and Queue/3.1 Three In One/ThreeInOne.py
@@ -1,4 +1,3 @@
-""" ThreeInOne """
 # Three Stack in Single Array
 arr = [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1]
 
@@ -7,17 +6,6 @@ tops = [-1, -1, -1]
 
 
 def push(stack_number: int, element: int) -> int:
-    """
-    Pushes an element onto a specific stack in the array-based implementation of multiple stacks.
-
-    Parameters:
-        stack_number (int): The index of the stack to push the element onto.
-        element (int): The element to be pushed onto the stack.
-
-    Returns:
-        int: Returns -1 if the stack is full and the element cannot be pushed.
-        Otherwise, returns the index position of the element in the array.
-    """
     if arr[-1] != -1:
         print("Full stack. Cannot insert.")
         return -1
@@ -38,15 +26,6 @@ def push(stack_number: int, element: int) -> int:
 
 
 def pop(stack_number: int) -> int:
-    """
-    Remove and return the top element from the specified stack.
-
-    Parameters:
-        stack_number (int): The number of the stack to pop from.
-
-    Returns:
-        int: The value of the popped element, or -1 if the stack is empty.
-    """
     if tops[stack_number] in [-1, tops[stack_number - 1]]:
         print("Empty Stack")
         return -1
@@ -66,15 +45,6 @@ def pop(stack_number: int) -> int:
 
 
 def peek(stack_number: int) -> int:
-    """
-    Returns the top element of the specified stack in the stack array.
-
-    Parameters:
-    - stack_number (int): The index of the stack to peek.
-
-    Returns:
-    - int: The top element of the specified stack, or -1 if the stack is empty.
-    """
     return (
         arr[tops[stack_number]]
         if tops[stack_number] not in [-1, tops[stack_number - 1]]
@@ -82,17 +52,5 @@ def peek(stack_number: int) -> int:
     )
 
 
-print(pop(0))
-push(0, 3)
-push(0, 2)
-push(2, 5)
-push(1, 8)
-push(1, 9)
-push(2, 1)
-push(0, 7)
-push(0, 11)
-print(pop(2))
-print(pop(2))
-print(pop(2))
-print(pop(0))
-print(peek(0), peek(1), peek(2))
+def is_empty(stack_number: int) -> bool:
+    return tops[stack_number] in [-1, tops[stack_number - 1]]

9d63684bc1001faa498fdd24e1674d57b583dc44
Kamal Singh <37046527+CodingStuff-GitHub@users.noreply.github.com>
2023-11-04 20:50:19 +0530

    Update ThreeInOne.py

--- 3. Stack and Queue/3.1 Three In One/ThreeInOne.py
+++ 3. Stack and Queue/3.1 Three In One/ThreeInOne.py
@@ -7,8 +7,19 @@ tops = [-1, -1, -1]
 
 
 def push(stack_number: int, element: int) -> int:
+    """
+    Pushes an element onto a specific stack in the array-based implementation of multiple stacks.
+
+    Parameters:
+        stack_number (int): The index of the stack to push the element onto.
+        element (int): The element to be pushed onto the stack.
+
+    Returns:
+        int: Returns -1 if the stack is full and the element cannot be pushed.
+        Otherwise, returns the index position of the element in the array.
+    """
     if arr[-1] != -1:
-        print("Full stack")
+        print("Full stack. Cannot insert.")
         return -1
     # This will be its index position
     position = tops[stack_number] + 1
@@ -23,9 +34,19 @@ def push(stack_number: int, element: int) -> int:
         tops[i] += 1
     print(arr)
     print(tops)
+    return 0
 
 
-def pop(stack_number: int):
+def pop(stack_number: int) -> int:
+    """
+    Remove and return the top element from the specified stack.
+
+    Parameters:
+        stack_number (int): The number of the stack to pop from.
+
+    Returns:
+        int: The value of the popped element, or -1 if the stack is empty.
+    """
     if tops[stack_number] in [-1, tops[stack_number - 1]]:
         print("Empty Stack")
         return -1
@@ -45,6 +66,15 @@ def pop(stack_number: int):
 
 
 def peek(stack_number: int) -> int:
+    """
+    Returns the top element of the specified stack in the stack array.
+
+    Parameters:
+    - stack_number (int): The index of the stack to peek.
+
+    Returns:
+    - int: The top element of the specified stack, or -1 if the stack is empty.
+    """
     return (
         arr[tops[stack_number]]
         if tops[stack_number] not in [-1, tops[stack_number - 1]]
